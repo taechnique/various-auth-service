8f4a74ca0923c626c51fa90bcb532ab3d1dc9ed4
Kimchi-dev <tnine923@gmail.com>
2022-05-23 17:02:21 +0900

    create dto

--- src/main/java/io/teach/user/dto/AuthRequestDto.java
+++ src/main/java/io/teach/user/dto/AuthRequestDto.java
@@ -2,9 +2,18 @@ package io.teach.user.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.teach.user.dto.model.UserAccountModel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class AuthRequestDto {
 
     @JsonProperty("user_account")
     private UserAccountModel userAccountModel;
+
+    @Override
+    public String toString() {
+        return String.format("{\n\t\"user_account\": %s\n}", userAccountModel);
+    }
 }
--- src/main/java/io/teach/user/dto/model/UserAccountModel.java
+++ src/main/java/io/teach/user/dto/model/UserAccountModel.java
@@ -1,4 +1,22 @@
 package io.teach.user.dto.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserAccountModel {
+
+
+    private String username;
+
+    @JsonInclude(Include.NON_EMPTY)
+    private String password;
+
+    @Override
+    public String toString() {
+        return String.format("{\n\t\"username\": %s,\n\t\"password\": %s\n}", this.username, this.password);
+    }
 }
